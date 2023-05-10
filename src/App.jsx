3e669c0a7f8e2f080222ddc68aa166d921773fa7
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-05-10 03:33:51 -0300

    feat: Component Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Container';
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </ul>
+
+      <Container customClass="minHeight">
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/company' element={<Company/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/newproject' element={<NewProject/>}/>
+        </Routes>
+      </Container>
+      <p>footer</p>
+    </Router>
   );
 }
 
