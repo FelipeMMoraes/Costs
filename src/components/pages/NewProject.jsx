bc4ce7ac6c768dbc521fe341a5ca73553c908bb5
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-05-15 09:43:28 -0300

    feat: page newproject CSS

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,5 +1,12 @@
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProject.module.css'
+
 export default function NewProject(){
   return(
-    <h1>Novo Projeto</h1>
+    <div className={styles.newprojectContainer}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm />
+    </div>
   )
 }
