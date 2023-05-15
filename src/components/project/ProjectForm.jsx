fa2f33681ba8e17b1781188707471d1678e32fa0
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-05-15 10:52:17 -0300

    connect project DB

--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react'
+
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
+
 export default function ProjectForm({ btnText }){
+
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data)
+      })
+      .catch(err => console.log(err))
+    }, [])
+
   return (
     <form className={styles.form}>
       <Input
@@ -17,7 +37,7 @@ export default function ProjectForm({ btnText }){
         name="budget"
         placeholder="Insira o orçamento total"/>
       <div>
-        <Select name="category_id" text="Selecione a categoria"/>
+        <Select name="category_id" text="Selecione a categoria" option={categories}/>
       </div>
       <SubmitButton text={btnText}/>
     </form>
